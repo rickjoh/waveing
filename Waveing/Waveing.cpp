a03f1d29b9835a43cabb6bd84906e0fc87436a0b
rickard <rickard.s.m.johansson@gmail.com>
2023-02-02 23:22:12 +0100

    initial commit and stuffs

---
+++ Waveing/Waveing.cpp
@@ -0,0 +1,14 @@
+﻿// Waveing.cpp : Defines the entry point for the application.
+//
+
+#include "Waveing.h"
+
+#include "stb_vorbis.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
